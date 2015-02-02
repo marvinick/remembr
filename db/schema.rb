062bb35b99ff81bceb2a86c79b62cbc6130ee671
marvinick <marviluw@gmail.com>
2015-02-01 22:32:48 -0600

    added header

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202033122) do
+ActiveRecord::Schema.define(version: 20150202034334) do
+
+  create_table "pages", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
