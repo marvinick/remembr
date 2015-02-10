0b7f5a8db6b4c5ca1e5b16b8c5c73cbf1e5ab170
marvinick <marviluw@gmail.com>
2015-02-09 18:45:53 -0600

    cancelled transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,24 +1,5 @@
 class TransactionsController < ApplicationController
   def create
-    membership = Membership.find_by!(slug: params[:slug])
-    token = params[:stripeToken]
 
-    begin
-      charge = Stripe::Charge.create (
-        amount: membership.price,
-        currency: "usd",
-        card: token,
-        description: current_user.email
-      )
-
-    @membership = Membership.create!(member_email: current_user.email)
-    redirect_to root_path
-
-    rescue Stripe::CardError => e
-
-      @error = e
-
-      redirect_to root_path, notice: @error
-    end
   end
 end
