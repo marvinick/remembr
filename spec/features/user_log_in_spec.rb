249ce2332ed9f8c94ef073900a93e3a3208f9e36
marvinick <marviluw@gmail.com>
2015-02-19 21:42:19 -0600

    feature test

--- spec/features/user_log_in_spec.rb
+++ spec/features/user_log_in_spec.rb
@@ -2,5 +2,9 @@ require 'spec_helper'
 
 feature 'User log in' do
   scenario "invalid credentials" do
+    visit 'users/sign_in path'
+    fill_in ("Name", with:"daddy")
+    click_button "Log in"
+    expect (current_path).to_eq(users/sign_in path)
   end
 end
